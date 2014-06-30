2631cbd555fef80197e96c2a0d945f763ef8d39e
Victor Lourng <hello@victorlourng.com>
2014-06-30 00:14:59 -0400

    Schedule layout and smooth scrolling

---
+++ scripts/site.js
@@ -0,0 +1,21 @@
+// Navigation
+function toggleMenu() {
+  $("body").toggleClass("nav-push");
+  $("nav").toggleClass("nav-open");
+}
+
+// jQuert
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
